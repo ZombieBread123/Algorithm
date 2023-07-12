e829bd5d8e4591405893dd5898b08da7389d8df1
ZomSik <sunberbee@naver.com>
2023-07-12 18:24:21 +0900

    11399 done

--- C++/11399.cpp
+++ C++/11399.cpp
@@ -1,9 +1,29 @@
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
+bool desc(int a, int b) {
+    return a < b;
+}
+
 int main() {
+    int n;
+    cin >> n;
+    int arr[n];
+
+    for (int i = 0; i < n; i++) 
+        cin >> arr[i];
     
+    sort(arr, arr + n, desc);
+
+    int cnt = 0;
+    int sum = 0;
+    for (int i = 0; i < n; i++) {
+        cnt += arr[i];
+        sum += cnt;
+    }
+    cout << sum;
 
     return 0;
 }
